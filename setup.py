f5bfda0a1da7f80c99822674477993cf77ed26b6
shawn.burke@teamburke.com <Correct horse battery Staple>
2019-04-12 21:16:40 -0700

    #9 check-in setup.py file.  Added run configuration for Pycharm to build the setup distribution package, called a wheel.

---
+++ setup.py
@@ -0,0 +1,25 @@
+# Original guidance from http://go.chriswarrick.com/entry_points
+# Also thanks to http://the-hitchhikers-guide-to-packaging.readthedocs.io/en/latest/creation.html
+# https://setuptools.readthedocs.io/en/latest/setuptools.html
+
+from setuptools import setup, find_packages
+
+setup(author='Shawn J Burke',
+      author_email='pypi.python@teamburke.com',
+      description='Python Project & Coding Standards for Practicing Wizards',
+      entry_points={
+          'console_scripts': ['py_guide = py_guide.__main__:main']
+        },
+      license='GNU GENERAL PUBLIC LICENSE',
+      # newline separates Description: header in PKG-INFO from readme content
+      long_description='\n' + open('README.rst').read(),
+      name='sjb.py_guide',
+      packages=find_packages("py_guide"),
+      project_urls={
+        "Bug Tracker": "https://github.com/shawnjburke/py_guide/issues/",
+        "Documentation": "https://shawnjburke.github.io/py_guide/",
+        "Source Code": "https://github.com/shawnjburke/py_guide/",
+      },
+      url="https://github.com/shawnjburke/py_guide",
+      version='2019.4.6',
+      )
