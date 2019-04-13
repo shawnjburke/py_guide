828535db2cb02455958cc1862a584ddf758e18ba
shawn.burke@teamburke.com <Correct horse battery Staple>
2019-04-12 22:14:42 -0700

    Added remarks (comments) to script to explain it's usage.

--- sphinx_all_builds.bat
+++ sphinx_all_builds.bat
@@ -1,3 +1,5 @@
+REM This script will compile all the themes used in documentation.  Since this project is a best practices
+REM example, multiple themes are compiled to show the power of Spinx documentation.
 venv\Scripts\python.exe "C:\Program Files\JetBrains\PyCharm Community Edition 2018.2.4\helpers\rest_runners\sphinx_runner.py" -b html docs-source docs
 venv\Scripts\python.exe "C:\Program Files\JetBrains\PyCharm Community Edition 2018.2.4\helpers\rest_runners\sphinx_runner.py" -b html -D html_theme=classic docs-source docs\themes\classic
 venv\Scripts\python.exe "C:\Program Files\JetBrains\PyCharm Community Edition 2018.2.4\helpers\rest_runners\sphinx_runner.py" -b html -D html_theme=sphinxdoc docs-source docs\themes\sphinxdoc
